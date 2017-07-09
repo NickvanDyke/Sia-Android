a6dc4d2cd5031c5b4fa6d9a1efc1dfbf4a4505dc
NickvanDyke <nickjvandyke15@gmail.com>
2017-07-09 03:11:45 -0500

    Add receiver to start services on boot and app update

---
+++ app/src/main/java/vandyke/siamobile/backend/BootReceiver.java
@@ -0,0 +1,15 @@
+package vandyke.siamobile.backend;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import vandyke.siamobile.misc.SiaMobileApplication;
+
+public class BootReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        if (SiaMobileApplication.prefs.getString("operationMode", "cold_storage").equals("local_full_node"))
+            context.startService(new Intent(context, SiadMonitor.class));
+        if (SiaMobileApplication.prefs.getBoolean("monitorInBackground", true))
+            context.startService(new Intent(context, WalletService.class));
+    }
+}
