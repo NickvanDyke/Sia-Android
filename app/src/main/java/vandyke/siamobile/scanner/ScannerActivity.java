ed71b39a12cf341f40df216544bdad7a55eee588
Jason Gallagher <jasongallagher21@gmail.com>
2017-07-13 16:12:15 -0400

    add runtime permissions for camera

--- app/src/main/java/vandyke/siamobile/scanner/ScannerActivity.java
+++ app/src/main/java/vandyke/siamobile/scanner/ScannerActivity.java
@@ -1,11 +1,13 @@
 package vandyke.siamobile.scanner;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.zxing.Result;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import vandyke.siamobile.R;
@@ -21,6 +23,8 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
         super.onCreate(state);
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
+        new RxPermissions(this).request(Manifest.permission.CAMERA)
+                .subscribe(this::onPermissionResult);
     }
 
     @Override
@@ -34,6 +38,14 @@ public class ScannerActivity extends AppCompatActivity implements ZXingScannerVi
     public void onPause() {
         super.onPause();
         scannerView.stopCamera();           // Stop camera on pause
+    }
+
+    public void onPermissionResult(boolean granted) {
+        if(!granted) {
+            Intent returnIntent = new Intent();
+            setResult(Activity.RESULT_CANCELED, returnIntent);
+            finish();
+        }
     }
 
     @Override
