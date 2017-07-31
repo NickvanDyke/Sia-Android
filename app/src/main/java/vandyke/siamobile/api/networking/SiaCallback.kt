fd7c77fed79753d81a4699852472ed3f39280dce
NickvanDyke <nickjvandyke15@gmail.com>
2017-07-31 16:41:33 +0000

    in progress

--- app/src/main/java/vandyke/siamobile/api/networking/SiaCallback.kt
+++ app/src/main/java/vandyke/siamobile/api/networking/SiaCallback.kt
@@ -4,15 +4,18 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SiaCallback<T>(val onSuccess: (Response<T>) -> Unit, val onError: (SiaError) -> Unit) : Callback<T> {
+class SiaCallback<T>(val onSuccess: (Response<T>) -> Unit, val onError: (SiaError<T>) -> Unit) : Callback<T> {
     override fun onResponse(call: Call<T>, response: Response<T>) {
         if (response.isSuccessful)
             onSuccess(response)
-        else
-            onError(SiaError())
+        else {
+            onError(SiaError(response))
+        }
     }
 
-    override fun onFailure(call: Call<T>?, t: Throwable?) {
-
+    override fun onFailure(call: Call<T>?, t: Throwable) {
+        onError(SiaError(t))
     }
+
+    // TODO: I'm assuming params in onResponse and onFailure aren't null, not sure if they actually can be. will find out
 }
--- app/src/main/java/vandyke/siamobile/api/networking/SiaError.kt
+++ app/src/main/java/vandyke/siamobile/api/networking/SiaError.kt
@@ -1,50 +1,47 @@
 package vandyke.siamobile.api.networking
 
 import retrofit2.Response
+import java.net.SocketTimeoutException
 
-class SiaError<T>(response: Response<T>) {
+class SiaError {
     val reason: Reason
 
-    init {
-        if (!response.isSuccessful)
-            reason = determineReason(response.errorBody().toString())
+    constructor(response: Response) {
+        reason = getReasonFromMsg(response.errorBody().toString())
     }
 
+    constructor(t: Throwable) {
+        reason = getReasonFromThrowable(t)
+    }
 
-    fun determineReason(errorMessage: String): SiaError.Reason {
-        if (errorMessage.contains("walletModel must be unlocked before it can be used"))
-            return SiaError.Reason.WALLET_LOCKED
-        else if (errorMessage.contains("provided encryption key is incorrect"))
-            return SiaError.Reason.WALLET_PASSWORD_INCORRECT
-        else if (errorMessage.contains("walletModel has already been unlocked"))
-            return SiaError.Reason.WALLET_ALREADY_UNLOCKED
-        else if (errorMessage.contains("could not read 'amount'"))
-            return SiaError.Reason.INVALID_AMOUNT
-        else if (errorMessage.contains("a password must be provided to newpassword"))
-            return SiaError.Reason.NO_NEW_PASSWORD
-        else if (errorMessage.contains("could not read address"))
-            return SiaError.Reason.COULD_NOT_READ_ADDRESS
-        else if (errorMessage.contains("transaction cannot have an output or payout that has zero value"))
-            return SiaError.Reason.AMOUNT_ZERO
-        else if (errorMessage.contains("unable to fund transaction"))
-            return SiaError.Reason.INSUFFICIENT_FUNDS
-        else if (errorMessage.contains("walletModel is already encrypted, cannot encrypt again"))
-            return SiaError.Reason.EXISTING_WALLET
-        else if (errorMessage.contains("API authentication failed"))
-            return SiaError.Reason.INCORRECT_API_PASSWORD
-        else if (errorMessage.contains("another walletModel rescan is already underway"))
-            return SiaError.Reason.WALLET_SCAN_IN_PROGRESS
-        else if (errorMessage.contains("walletModel has not been encrypted yet"))
-            return SiaError.Reason.WALLET_NOT_ENCRYPTED
-        else if (errorMessage.contains("cannot init from seed until blockchain is synced"))
-            return SiaError.Reason.CANNOT_INIT_FROM_SEED_UNTIL_SYNCED
-        else if (errorMessage.contains("unsupported on cold storage walletModel"))
-            return SiaError.Reason.UNSUPPORTED_ON_COLD_WALLET
-        else if (errorMessage.contains("word not found in dictionary for given language"))
-            return SiaError.Reason.INVALID_WORD_IN_SEED
-        else {
-            println(errorMessage)
-            return SiaError.Reason.UNACCOUNTED_FOR_ERROR
+    fun getReasonFromMsg(errorMessage: String): SiaError.Reason {
+        when {
+            errorMessage.contains("walletModel must be unlocked before it can be used") -> return SiaError.Reason.WALLET_LOCKED
+            errorMessage.contains("provided encryption key is incorrect") -> return SiaError.Reason.WALLET_PASSWORD_INCORRECT
+            errorMessage.contains("walletModel has already been unlocked") -> return SiaError.Reason.WALLET_ALREADY_UNLOCKED
+            errorMessage.contains("could not read 'amount'") -> return SiaError.Reason.INVALID_AMOUNT
+            errorMessage.contains("a password must be provided to newpassword") -> return SiaError.Reason.NO_NEW_PASSWORD
+            errorMessage.contains("could not read address") -> return SiaError.Reason.COULD_NOT_READ_ADDRESS
+            errorMessage.contains("transaction cannot have an output or payout that has zero value") -> return SiaError.Reason.AMOUNT_ZERO
+            errorMessage.contains("unable to fund transaction") -> return SiaError.Reason.INSUFFICIENT_FUNDS
+            errorMessage.contains("walletModel is already encrypted, cannot encrypt again") -> return SiaError.Reason.EXISTING_WALLET
+            errorMessage.contains("API authentication failed") -> return SiaError.Reason.INCORRECT_API_PASSWORD
+            errorMessage.contains("another walletModel rescan is already underway") -> return SiaError.Reason.WALLET_SCAN_IN_PROGRESS
+            errorMessage.contains("walletModel has not been encrypted yet") -> return SiaError.Reason.WALLET_NOT_ENCRYPTED
+            errorMessage.contains("cannot init from seed until blockchain is synced") -> return SiaError.Reason.CANNOT_INIT_FROM_SEED_UNTIL_SYNCED
+            errorMessage.contains("unsupported on cold storage walletModel") -> return SiaError.Reason.UNSUPPORTED_ON_COLD_WALLET
+            errorMessage.contains("word not found in dictionary for given language") -> return SiaError.Reason.INVALID_WORD_IN_SEED
+            else -> {
+                println(errorMessage)
+                return SiaError.Reason.UNACCOUNTED_FOR_ERROR
+            }
+        }
+    }
+
+    fun getReasonFromThrowable(t: Throwable): Reason {
+        when (t) {
+            is SocketTimeoutException -> return Reason.TIMEOUT
+            else -> return Reason.UNACCOUNTED_FOR_ERROR
         }
     }
 
